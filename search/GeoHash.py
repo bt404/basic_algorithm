6ec31cd4d797e2f39e8029c940e808eca69b39fa
knd2 <gjz.knd2@gmail.com>
2014-09-20 22:30:24 +0800

    GoeHash fix

--- search/GeoHash.py
+++ search/GeoHash.py
@@ -1,12 +1,18 @@
-def GeoHash(lat, lng):
-    max_lat = 180
-    min_lat = -180
-    max_lng = 90
-    min_lng = -90
-    if not (min_lat<=lat<=max_lat and min_lng<=lng<=max_lng):
+MAX_LAT = 180
+MIN_LAT = -180
+MAX_LNG = 90
+MIN_LNG = -90
+
+
+def GeoHash(lat, lng, times=26):
+    if not (MIN_LAT<=lat<=MAX_LAT and MIN_LNG<=lng<=MAX_LNG) or times<=0:
         return -1
+    max_lat = MAX_LAT
+    min_lat = MIN_LAT
+    max_lng = MAX_LNG
+    min_lng = MIN_LNG
     ret = ""
-    for i in xrange(26):
+    for i in xrange(times):
         if lat < (max_lat+min_lat)/2.0:
             second = '0'
             max_lat = (max_lat+min_lat)/2.0
@@ -30,6 +36,13 @@ if __name__ == "__main__":
     point = (-58, 29)
     hash_val = GeoHash(point[0], point[1])
     print hash_val
-
     print
+
     print len(hash_val)
+    print
+
+    point = (-58, 29)
+    hash_val = GeoHash(point[0], point[1], 2)
+    print hash_val
+    print len(hash_val)
+    print 
