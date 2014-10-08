defe49b8e15085ad9c5517b2d7875e42c71ff352
knd2 <gjz.knd2@gmail.com>
2014-10-08 14:13:48 +0800

    FindTopK bug fix k的比较

--- game/FindTopK.c
+++ game/FindTopK.c
@@ -29,23 +29,29 @@ int Partition(int data[], int start, int end)
 
 void GetTopK(int data[], int start, int end, int k)
 {
+    printf("start: %d\n", start);
+    printf("end: %d\n", end);
+    if(k <= 0)
+        return;
     int count = Partition(data, start, end);
+    printf("count: %d\n", count+1);
+    printf("\n");
     if(count == -1)
         return;
-    else if(count+1 > k)
+    else if((count-start+1) > k)    // k 是每次要找到的元素数
     {
         GetTopK(data, start, count-1, k);
     }
-    else if(count+1 < k)
+    else if((count-start+1) < k)
     {
-        GetTopK(data, count+1, end, k-count-1);
+        GetTopK(data, count+1, end, k-(count-start+1));
     }
 }
 
 int main(void)
 {
-    int data[] = {1, 32, 4, 2, 14, 9, 24, 7, 100, 54, 8};
-    int top_k = 7;
+    int data[] = {1, 32, 4, 2, 14, 9, 25, 7, 100, 54, 8};
+    int top_k = 8;
     GetTopK(data, 0, sizeof(data)/sizeof(int)-1, top_k);
     for(int i=0; i<top_k; ++i)
     {
