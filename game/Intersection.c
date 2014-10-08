faaa6c3e546e5a9f5ef9f7dc04ba93374ce3cfa0
knd2 <gjz.knd2@gmail.com>
2014-10-08 10:16:28 +0800

    求交集

---
+++ game/Intersection.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+void Intersection(int data1[], int size1, int data2[], int size2)
+{
+    if(data1==NULL || data2==NULL || size1<=0 || size2<=0)
+        return;
+    int i = 0;
+    int j = 0;
+    int size = size1>size2? size2: size1;
+    int data[size];
+    int k=0;
+    while(i<size1 && j<size2)
+    {
+        if(data1[i] == data2[j])
+        {
+            data[k] = data1[i];
+            ++k;
+            ++i;
+            ++j;
+        }
+        else if(data1[i] < data2[j])
+        {
+            ++i;
+        }
+        else
+        {
+            ++j;
+        }
+    }
+    for(i=0; i<k; ++i)
+    {
+        printf("%d ", data[i]);
+    }
+    printf("\n");
+}
+
+int main(void)
+{
+    int data1[] = {1, 2, 4, 6, 8, 12, 24};
+    int data2[] = {1, 2, 5, 7, 8, 12, 24, 30};
+    Intersection(data1, 7, data2, 8);
+    return 0;
+}
